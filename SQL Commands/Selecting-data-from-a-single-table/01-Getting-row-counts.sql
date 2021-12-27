fe0b49d6c2be37471636f196772645987ea20287
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-27 15:44:09 -0300

    Selecting data from a single database/ Getting row counts

---
+++ SQL Commands/Selecting-data-from-a-single-table/01-Getting-row-counts.sql
@@ -0,0 +1,8 @@
+use employees;
+select * from employees;
+select count(*) from employees;
+select count(*) from departments;
+select count(*) from dept_emp;
+select count(*) from dept_manager;
+select count(*) from salaries;
+select count(*) from titles;
