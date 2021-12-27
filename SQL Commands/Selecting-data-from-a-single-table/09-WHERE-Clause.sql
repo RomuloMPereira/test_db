1e0c324c72f31022f9ef54de4d3e2ba936bbfa2a
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-27 18:02:29 -0300

    WHERE Clause - IN and NOT IN

--- SQL Commands/Selecting-data-from-a-single-table/09-WHERE-Clause.sql
+++ SQL Commands/Selecting-data-from-a-single-table/09-WHERE-Clause.sql
@@ -12,3 +12,9 @@ SELECT count(*) FROM employees WHERE first_name = 'Elvis' AND gender = 'M';
 
 SELECT * FROM employees WHERE first_name = 'Elvis' AND last_name = 'Velasco';
 SELECT * FROM employees WHERE first_name = 'Elvis' OR last_name = 'Velasco';
+
+SELECT * FROM employees WHERE first_name IN ('Elvis');
+SELECT * FROM employees WHERE first_name IN ('Elvis', 'Sumant', 'Berni', 'Lillian');
+SELECT count(*) FROM employees WHERE first_name IN ('Elvis', 'Sumant', 'Berni', 'Lillian');
+SELECT count(*) FROM employees WHERE first_name IN ('Elvis', 'Sumant', 'Berni', 'Lillian')
+	AND last_name NOT IN ('Redmiles', 'Haddadi', 'Demeyer', 'Restivo');
