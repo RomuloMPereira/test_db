7395977c66ff599c635d3bf59379d104adcefbc3
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-27 17:54:07 -0300

    WHERE Clause - AND and OR

--- SQL Commands/Selecting-data-from-a-single-table/09-WHERE-Clause.sql
+++ SQL Commands/Selecting-data-from-a-single-table/09-WHERE-Clause.sql
@@ -6,3 +6,9 @@ SELECT * FROM employees WHERE first_name != 'Elvis';
 SELECT * FROM employees WHERE first_name <> 'Elvis';
 
 SELECT count(*) FROM employees WHERE first_name = 'Elvis';
+
+SELECT * FROM employees WHERE first_name = 'Elvis' AND gender = 'M';
+SELECT count(*) FROM employees WHERE first_name = 'Elvis' AND gender = 'M';
+
+SELECT * FROM employees WHERE first_name = 'Elvis' AND last_name = 'Velasco';
+SELECT * FROM employees WHERE first_name = 'Elvis' OR last_name = 'Velasco';
